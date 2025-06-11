adc871e71fcdbd721b5f68cb800d38a1a1bc398a
Joni Chandra <jonquihote@gmail.com>
2025-06-11 14:04:25 +0700

    chore: remove user resource info list

--- modules/home/app/Filament/Resources/UserResource.php
+++ modules/home/app/Filament/Resources/UserResource.php
@@ -3,11 +3,9 @@
 namespace Venture\Home\Filament\Resources;
 
 use Filament\Forms\Form;
-use Filament\Infolists\Infolist;
 use Filament\Resources\Resource;
 use Filament\Tables\Table;
 use Venture\Home\Filament\Resources\UserResource\InitializeFormSchema;
-use Venture\Home\Filament\Resources\UserResource\InitializeInfoSchema;
 use Venture\Home\Filament\Resources\UserResource\InitializeTableColumns;
 use Venture\Home\Filament\Resources\UserResource\Pages;
 use Venture\Home\Models\User;
@@ -28,11 +26,6 @@ class UserResource extends Resource
     public static function table(Table $table): Table
     {
         return InitializeTableColumns::run($table);
-    }
-
-    public static function infolist(Infolist $infolist): Infolist
-    {
-        return InitializeInfoSchema::run($infolist);
     }
 
     public static function getPages(): array
