1eb96e85203bc25c72702a9460b8fa55026ec9ed
Joni Chandra <jonquihote@gmail.com>
2025-07-11 09:48:03 +0700

    fix: login screen not being styled

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('login', function () {
+    return Redirect::route('filament.home.auth.login');
+})->name('login');
