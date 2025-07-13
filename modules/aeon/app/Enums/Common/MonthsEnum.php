094d29ece09571ebd07f2776faa9e9e5c45c9531
Joni Chandra <jonquihote@gmail.com>
2025-07-13 10:18:54 +0700

    chore: add months enum

---
+++ modules/aeon/app/Enums/Common/MonthsEnum.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Venture\Aeon\Enums\Common;
+
+enum MonthsEnum: string
+{
+    case JANUARY = 'january';
+    case FEBRUARY = 'february';
+    case MARCH = 'march';
+    case APRIL = 'april';
+    case MAY = 'may';
+    case JUNE = 'june';
+    case JULY = 'july';
+    case AUGUST = 'august';
+    case SEPTEMBER = 'september';
+    case OCTOBER = 'october';
+    case NOVEMBER = 'november';
+    case DECEMBER = 'december';
+}
