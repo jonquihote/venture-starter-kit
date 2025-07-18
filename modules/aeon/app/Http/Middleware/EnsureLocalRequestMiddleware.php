22915542f01ee822e3876030769135e58c9daecb
Joni Chandra <jonquihote@gmail.com>
2025-07-18 09:34:20 +0700

    feat: add local request middleware

---
+++ modules/aeon/app/Http/Middleware/EnsureLocalRequestMiddleware.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Venture\Aeon\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class EnsureLocalRequestMiddleware
+{
+    public function handle(Request $request, Closure $next)
+    {
+        $allowedIps = ['127.0.0.1', '::1'];
+
+        if (! in_array($request->ip(), $allowedIps)) {
+            abort(Response::HTTP_FORBIDDEN);
+        }
+
+        return $next($request);
+    }
+}
