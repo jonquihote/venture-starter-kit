3bb724c6ef5706b9c917756926ba57679be27158
Joni Chandra <jonquihote@gmail.com>
2025-07-18 11:17:26 +0700

    feat: set up export / import tables

--- modules/home/app/Providers/AuthServiceProvider.php
+++ modules/home/app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Venture\Home\Providers;
 
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
 use Venture\Home\Models\User;
@@ -11,6 +12,8 @@ class AuthServiceProvider extends ServiceProvider
     public function register(): void
     {
         Config::set('auth.providers.users.model', User::class);
+
+        $this->app->bind(Authenticatable::class, User::class);
     }
 
     public function boot(): void {}
