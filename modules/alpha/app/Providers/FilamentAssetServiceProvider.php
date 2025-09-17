b058eb44543461571ac23b349c8bf237200d3f2b
Joni Chandra <jonquihote@gmail.com>
2025-09-18 00:09:59 +0700

    chore: fix vite manifest not found

--- modules/alpha/app/Providers/FilamentAssetServiceProvider.php
+++ modules/alpha/app/Providers/FilamentAssetServiceProvider.php
@@ -4,6 +4,8 @@ namespace Venture\Alpha\Providers;
 
 use Filament\Support\Assets\Js;
 use Filament\Support\Facades\FilamentAsset;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
@@ -13,10 +15,15 @@ class FilamentAssetServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        FilamentAsset::register([
-            Js::make('livewire-echo', Vite::asset('resources/js/livewire-echo.ts'))->module(),
+        $assets = Collection::make([
             Js::make('tailwindplus-elements', 'https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1')->module(),
             Js::make('mermaid', 'https://cdn.jsdelivr.net/npm/mermaid@11/dist/mermaid.esm.min.mjs')->module(),
         ]);
+
+        if (File::exists(public_path('build/manifest.json'))) {
+            $assets->push(Js::make('livewire-echo', Vite::asset('resources/js/livewire-echo.ts'))->module());
+        }
+
+        FilamentAsset::register($assets->toArray());
     }
 }
