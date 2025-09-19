7dfbe85675a2782079e049bee96b7db616df21e0
Joni Chandra <jonquihote@gmail.com>
2025-09-19 09:48:16 +0700

    chore: redirect home page to login page by default

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,21 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+// Route::get('/', function () {
+//    return Inertia::render('Welcome');
+// })->name('home');
+
 Route::get('/', function () {
-    return Inertia::render('Welcome');
-})->name('home');
+    return redirect()->route('login');
+});
 
-Route::get('dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('login', function () {
+    return redirect()->route('filament.alpha.auth.login');
+})->name('login');
 
-require __DIR__ . '/settings.php';
-require __DIR__ . '/auth.php';
+// Route::get('dashboard', function () {
+//    return Inertia::render('Dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+//
+// require __DIR__ . '/settings.php';
+// require __DIR__ . '/auth.php';
